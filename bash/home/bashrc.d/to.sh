5c5cffcbed3c8a7c0ed569c6125fc39732e18d39
ypf791 <ypf791@gmail.com>
2018-03-29 00:13:49 +0800

    1. small fix on mkscript and bashrc 2. start implementation of to-bookmark

--- bash/home/bashrc.d/to.sh
+++ bash/home/bashrc.d/to.sh
@@ -1,55 +1,15 @@
 #!/bin/bash
 
 to() {
-	local ret=0
-	local isShowHelp=
-
-	OPTIND= OPTARG= opt=
-	while getopts "h" opt; do
-		if [ "${OPTARG#-}" != "$OPTARG" ]; then
-			echo "to: option requires an argument -- $opt" >&2
-			opt=?
-		fi
-		case $opt in
-			h)	isShowHelp=yes
-				;;
-			?)	isShowHelp=yes
-				ret=1
-				;;
-		esac
-	done
-
-	shift $((OPTIND-1))
-
-	if [ -n "$isShowHelp" ]; then
-		cat >&2 << EOF
-USAGE: to [-h] <back pattern>
-OPTIONS
-	-h	show this message
-ARGUMENTS
-	specify the back pattern of the destination directory.
-EOF
-		return $ret
+	if [ ! -x ~/.bashrc.d/src/to_int ]; then
+		echo "to: missing internal binary" >&2
+		return 255
 	fi
 
-	if [ -z "$1" ]; then
-		echo "to: must specify back pattern" >&2
-		return 1
+	cd_path=$(~/.bashrc.d/src/to_int "$@")
+	if [ 0 -eq $? -a -n "${cd_path}" ]; then
+		cd ${cd_path}
 	fi
-
-	pattern=$1
-	dir=`pwd`
-	while [ "/" != "$dir" ]; do
-		dir=`dirname $dir`
-		base=`basename $dir`
-		if echo "$base" | grep -q -- "^$pattern" >/dev/null 2>&1; then
-			echo "back to ${dir}"
-			cd $dir
-			return 0
-		fi
-	done
-
-	echo "to: not available back pattern" >&2
-	return 2
+	return $?
 }
 
