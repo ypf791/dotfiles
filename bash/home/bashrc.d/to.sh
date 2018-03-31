ae38eb3f54a63813306dc6e408204c37e8fcaad5
ypf791 <ypf791@gmail.com>
2018-04-01 00:24:52 +0800

    feature complete

--- bash/home/bashrc.d/to.sh
+++ bash/home/bashrc.d/to.sh
@@ -15,7 +15,7 @@ oo_to() {
 		return 0
 	fi
 
-	cd_path=$(~/.bashrc.d/src/to_int "$@")
+	local cd_path=$(~/.bashrc.d/src/to_int "$@")
 	if [ 0 -eq $? -a -n "${cd_path}" ]; then
 		cd ${cd_path}
 	fi
