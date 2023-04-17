c51624b375da3daee3780fc33fcfc9ca2e9bdcf1
ypf791 <ypf791@gmail.com>
2023-04-17 22:27:38 +0800

    zsh: prompt add branch

--- zsh/home/zshrc.d/prompt.sh
+++ zsh/home/zshrc.d/prompt.sh
@@ -1,8 +1,8 @@
 precmd()
 {
-	psvar=( OOVBMac "$(printf "%2x" "$?")" )
+	psvar=( OOVBMac "$(printf "%2x" "$?")" "$(git rev-parse --abbrev-ref HEAD 2>/dev/null)" )
 }
 
 PS1='
-%F{cyan}[%n@%1v %D{%H:%M}]%F{9}%0(?.%F{11}.)[%2v]%F{8}[%~]%f
+%F{cyan}[%n@%1v %D{%H:%M}]%F{9}%0(?.%F{11}.)[%2v]%F{12}[%~]%3(V.%F{8}[%3v].)%f
 $ '
